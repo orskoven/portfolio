56083d963acec2217b4232b8f4d32394613f89a7
Simon Pedersen <90264457+orskoven@users.noreply.github.com>
2022-02-23 10:48:36 +0100

    title hyperlink

--- src/main/java/com/example/portfolio/controllers/PortfolioController.java
+++ src/main/java/com/example/portfolio/controllers/PortfolioController.java
@@ -10,4 +10,8 @@ public class PortfolioController {
     public String getFrontPage(){
         return "index";
     }
+    @GetMapping("/imsimon")
+    public String getImsimon(){
+        return "imsimon";
+    }
 }
