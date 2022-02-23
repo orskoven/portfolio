86e89d28d5728076b3139b1b17333ff309878046
Simon Pedersen <90264457+orskoven@users.noreply.github.com>
2022-02-23 18:25:39 +0100

    sound

--- src/main/java/com/example/portfolio/controllers/PortfolioController.java
+++ src/main/java/com/example/portfolio/controllers/PortfolioController.java
@@ -1,6 +1,5 @@
 package com.example.portfolio.controllers;
 
-import com.example.portfolio.repositories.SoundPlayer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -10,7 +9,6 @@ public class PortfolioController {
 
     @GetMapping("/")
     public String getFrontPage(){
-        new SoundPlayer("src/main/resources/the9th.wav");
         return "index";
     }
     @GetMapping("/imsimon")
